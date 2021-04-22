886f622b0690371ad89606ecf2752808ed212fbc
Richard Mills <r.mills@nchain.com>
2021-04-22 11:40:40 +0100

    version 1.0.7.1

--- sv/0.2.1/docker-entrypoint.sh
+++ sv/0.2.1/docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+core_pattern=$(cat /proc/sys/kernel/core_pattern)
+pattern_start="$(echo $core_pattern | head -c 1)"
+
+# In case core pattern doesn't start with pipe make sure that specified
+# directory exists and has sufficient permissions to write core dump.
+if [[ $pattern_start != "|" ]]; then
+	directory=`dirname "$core_pattern"`
+	mkdir -p "$directory"
+	chmod 777 "$directory"
+fi
 
 if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1" == "test_bitcoin" ]]; then
 	mkdir -p "$BITCOIN_DATA"
@@ -11,6 +21,8 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 		rpcallowip=::/0
 		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+		excessiveblocksize=1000000000
+		maxstackmemoryusageconsensus=100000000
 		EOF
 		chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
 	fi
